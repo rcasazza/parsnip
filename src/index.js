e1de29a9655f2c3d21ebc004c82c389e1eed1a53
Bob Casazza <rcasazza@gmail.com>
2021-06-28 18:45:35 -0400

    introduce redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import tasks from './reducers';
+
+const store = createStore(tasks);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
